69bca538fd8b5eb4a223302952d7e80c13c72d25
Velorexe <david.bruschke@gmail.com>
2018-09-26 10:08:23 +0200

    Removed Unity, created Visual Studio project

--- Hotel [Unity]/Hotel/Assets/Scripts/HotelManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using HotelEvents;
-
-public class HotelManager : MonoBehaviour {
-    HotelManager hotel = new HotelManager();
-    
-}
