2664433e72b5eeb21f50eb4d23ae0a6ba903da86
Velorexe <david.bruschke@gmail.com>
2018-09-28 15:30:07 +0200

    Added room classes and interfaces

---
+++ HotelSimulatie/HotelSimulatie/Classes/Hotel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSimulatie
+{
+    public class Hotel
+    {
+        List<Floor> Floors = new List<Floor>();
+    }
+    class Floor
+    {
+        public int FloorHeight { get; set; }
+        public List<IArea> Rooms { get; set; }
+    }
+}
--- HotelSimulatie/HotelSimulatie/SimulationForm.cs
+++ HotelSimulatie/HotelSimulatie/SimulationForm.cs
@@ -15,6 +15,8 @@ namespace HotelSimulatie
         public SimulationForm(string fileLocation)
         {
             InitializeComponent();
+            ImportLayout import = new ImportLayout();
+            Hotel hotel = import.LayoutImport(fileLocation);
         }
     }
 }
