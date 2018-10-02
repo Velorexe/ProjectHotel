8a9b1da6289e2ddcd37c53e67bd65ca3406caf54
unknown <17054125@student.hhs.nl>
2018-10-02 12:42:46 +0200

    Added things

---
+++ Hotel [Unity]/Hotel/Assets/Scripts/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customer : MonoBehaviour {
+
+    public int Stay { get; set; }
+    public enum Status
+    {
+
+    }
+
+}
---
+++ Hotel [Unity]/Hotel/Assets/Scripts/Room Scripts/Room.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : IRoom {
+
+	public int Classification { get; set; }
+    public string AreaType { get; set; }
+    public int PositionX { get; set; }
+    public int PositionY { get; set; }
+    public int DimentionX { get; set; }
+    public int DimentionY { get; set; }
+}
