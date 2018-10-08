857a34f0a5f7f85c13db3517d8f65f57f4027b75
Luuk van Marsbergen <17054125@student.hhs.nl>
2018-10-08 16:09:28 +0200

    added output folder

---
+++ HotelSimulatie/HotelSimulatie/Classes/Output/DummyEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSimulatie
+{
+    class DummyEvent
+    {
+        public string Message { get; set; }
+        public string EventType { get; set; }
+        public string merginproblems { get; set; }
+    }
+}
