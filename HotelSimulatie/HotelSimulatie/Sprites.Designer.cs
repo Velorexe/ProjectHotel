e8d57e924661d2619776077c78604ececb97fdfd
Luuk van Marsbergen <17054125@student.hhs.nl>
2018-10-09 15:14:48 +0200

    Added Customer Sprite

--- HotelSimulatie/HotelSimulatie/Sprites.Designer.cs
+++ HotelSimulatie/HotelSimulatie/Sprites.Designer.cs
@@ -73,6 +73,16 @@ namespace HotelSimulatie {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Customer {
+            get {
+                object obj = ResourceManager.GetObject("Customer", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Elevator {
             get {
                 object obj = ResourceManager.GetObject("Elevator", resourceCulture);
